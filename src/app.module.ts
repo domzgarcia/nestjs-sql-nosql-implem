c2b558b66351f3b3de2709652ac402ac386dfb4e
bb_domgarcia <dom.garcia@nuworks.ph>
2019-08-15 18:52:53 +0800

    addded mongodb jwt passport integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,19 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { join } from 'path';
 import { AppController } from './app.controller';
+import { ApiController } from './api.controller'
 import { AppService } from './app.service';
 // import { PhotoModule } from './photo/photo.module';
 import { CatsModule } from './cat/cats.module'
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 @Module({
   imports: [
     CatsModule,
+    AuthModule,
+    UsersModule,
   ],
-  controllers: [AppController],
+  controllers: [AppController, ApiController],
   providers: [AppService],
 })
 export class AppModule {}
